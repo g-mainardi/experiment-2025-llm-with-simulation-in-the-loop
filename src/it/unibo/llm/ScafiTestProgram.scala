8b959c13b72f5723e0265fb92b66f9801a3c9030
Nicolas Farabegoli <nicolas.farabegoli@gmail.com>
2025-10-15 16:20:54 +0200

    remove boilerplate

--- src/it/unibo/llm/ScafiTestProgram.scala
+++ src/it/unibo/llm/ScafiTestProgram.scala
@@ -3,7 +3,6 @@ package it.unibo.llm
 import it.unibo.alchemist.model.scafi.ScafiIncarnationForAlchemist._
 
 class ScafiTestProgram extends AggregateProgram with StandardSensors with ScafiAlchemistSupport with BlockG with BlockC with BlockS {
-  private lazy val temp = randomGenerator().nextDouble() * 100
   override def main(): Any = {
   }
 }
