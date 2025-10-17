2bc6afd7aaae1b154dfa4373359702d696d07aac
Nicolas Farabegoli <nicolas.farabegoli@gmail.com>
2025-10-17 09:47:17 +0200

    setup simulation tool

--- mcp_server/src/it/unibo/llm/mcp/server/Server.scala
+++ mcp_server/src/it/unibo/llm/mcp/server/Server.scala
@@ -8,8 +8,8 @@ import io.modelcontextprotocol.spec.McpSchema.{CallToolRequest, CallToolResult, 
 import it.unibo.llm.mcp.server.utils.ScafiTestUtils
 import reactor.core.publisher.Mono
 
+import scala.concurrent.duration.Duration
 import scala.io.Source
-import scala.jdk.CollectionConverters._
 
 class Server {
   private val mapper = new JacksonMcpJsonMapper(new ObjectMapper())
@@ -21,7 +21,8 @@ class Server {
     .logging()
     .build()
   private val compilationSchema = Source.fromResource("schemas/compilation_schema.json").mkString
-  private val toolSpecification = new McpServerFeatures.AsyncToolSpecification(
+  private val simulationSchema = Source.fromResource("schemas/simulation_schema.json").mkString
+  private val compilationTool = new McpServerFeatures.AsyncToolSpecification(
     Tool.builder()
       .title("Code Compilation Tool")
       .description("A tool to compile and check Scala code snippets for errors.")
@@ -31,16 +32,36 @@ class Server {
     null,
     handleCompilation,
   )
+  private val simulationTool = new McpServerFeatures.AsyncToolSpecification(
+    Tool.builder()
+      .title("Code Simulation Tool")
+      .description("A tool to simulate ScaFi programs and check for runtime errors.")
+      .name("simulate_code")
+      .inputSchema(mapper, simulationSchema)
+      .build(),
+    null,
+    handleSimulation,
+  )
 
   private def handleCompilation(server: McpAsyncServerExchange, request: CallToolRequest): Mono[CallToolResult] = {
     val (hasErrors, errors) = ScafiTestUtils.compileAndGetErrors(request.arguments().get("program").toString)
     Mono.just(new CallToolResult(if (!hasErrors) { "compilation success" } else { errors.mkString("\n") }, hasErrors))
   }
 
+  private def handleSimulation(server: McpAsyncServerExchange, request: CallToolRequest): Mono[CallToolResult] = {
+    val program = request.arguments().get("program").toString
+    val timeout = request.arguments().get("timeout").toString
+    val durationTimeout = try Duration(s"${timeout}s") catch {
+      case _: NumberFormatException => return Mono.just(new CallToolResult(s"Invalid timeout format: $timeout", true))
+    }
+    val (hasErrors, errors) = ScafiTestUtils.simulateProgram(program, durationTimeout)
+    Mono.just(new CallToolResult(if (!hasErrors) { "simulation success" } else { errors.mkString("\n") }, hasErrors))
+  }
+
   def initialize(): Unit = {
     McpServer.async(transport)
       .capabilities(capabilities)
-      .tools(toolSpecification)
+      .tools(compilationTool, simulationTool)
       .build()
     ()
   }
--- mcp_server/src/it/unibo/llm/mcp/server/utils/ScafiTestUtils.scala
+++ mcp_server/src/it/unibo/llm/mcp/server/utils/ScafiTestUtils.scala
@@ -1,13 +1,18 @@
 package it.unibo.llm.mcp.server.utils
 
+import java.nio.file.Files
+import scala.concurrent.duration.Duration
 import scala.tools.nsc.{Global, Settings}
 import scala.tools.nsc.reporters.StoreReporter
 import scala.reflect.internal.util.BatchSourceFile
 
 object ScafiTestUtils {
   def compileAndGetErrors(code: String): (Boolean, List[String]) = {
+    val tempDir = Files.createTempDirectory("scafi-compile-")
     val settings = new Settings()
     settings.usejavacp.value = true
+
+    settings.outputDirs.setSingleOutput(tempDir.toFile.toString)
 
     val reporter = new StoreReporter(settings)
     val compiler = new Global(settings, reporter)
@@ -22,7 +27,11 @@ object ScafiTestUtils {
       ).toList
       (reporter.hasErrors, errors)
     } catch {
-      case e: Exception => (false, List(s"Compilation error: ${e.getMessage}"))
+      case e: Exception => (true, List(s"Compilation error: ${e.getMessage}"))
     }
+  }
+
+  def simulateProgram(program: String, timeout: Duration): (Boolean, List[String]) = {
+    ???
   }
 }
