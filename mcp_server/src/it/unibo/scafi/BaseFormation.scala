5fa7cf8a355ff46c677ef5df9ef6136da137f951
Nicolas Farabegoli <nicolas.farabegoli@gmail.com>
2025-10-20 10:45:25 +0200

    push broken code for @cric96

---
+++ mcp_server/src/it/unibo/scafi/BaseFormation.scala
@@ -0,0 +1,17 @@
+package it.unibo.scafi
+
+
+trait BaseFormation extends AggregateProgram with StandardSensors with BlockG with BlockC  {
+  def distanceVector: (Double, Double) = nbrvar(NBR_VECTOR)
+
+  def module(position: (Double, Double)): Double =
+    Math.sqrt(position._1 * position._1 + position._2 * position._2)
+
+  def normalize(position: (Double, Double)): (Double, Double) = {
+    val module = this.module(position)
+    (position._1 / module, position._2 / module)
+  }
+
+  def rotate90(position: (Double, Double)): (Double, Double) =
+    (-position._2, position._1)
+}
---
+++ mcp_server/src/it/unibo/scafi/PointTheLeader.scala
@@ -0,0 +1,8 @@
+package it.unibo.scafi
+
+class PointTheLeader extends BaseFormation {
+
+  override def main(): Any = align(getClass) {
+    val distance =
+  }
+}
